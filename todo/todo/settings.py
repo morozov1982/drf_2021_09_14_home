e2de820595fd775808d0de1892af01d17db4691b
morozov1982 <morozov1982@mail.com>
2021-10-11 13:31:13 +0600

    add: lesson_08 homework

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -68,7 +68,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',  # немного больше понравился
     'PAGE_SIZE': 100,  # ;-) раз в задании так, значит оставляем 100 ;-)
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
