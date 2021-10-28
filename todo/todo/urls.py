21989a23f73f102018dd829600533296589f8391
Morozov Alexandr <morozov1982@gmail.com>
2021-10-28 14:07:10 +0600

    add: lesson_12 homework complete

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.permissions import AllowAny
@@ -38,4 +39,5 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
     path('graphql/', GraphQLView.as_view(graphiql=True)),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
